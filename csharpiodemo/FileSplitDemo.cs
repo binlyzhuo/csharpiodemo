c4ad1f521b4f109c9ff875d686d781483245edd0
binlyzhuo <binlyzhuo@outlook.com>
2018-09-15 12:03:08 +0800

    00

--- csharpiodemo/FileSplitDemo.cs
+++ csharpiodemo/FileSplitDemo.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace csharpiodemo
 {
+    //https://www.cnblogs.com/yy3b2007com/p/5558877.html
+    //https://www.cnblogs.com/MRRAOBX/articles/6184722.html
+    //https://www.cnblogs.com/Olive116/archive/2012/09/06/WinForm.html
+    //https://blog.csdn.net/flyking2/article/details/78991774
     public class FileSplitDemo
     {
         public static void Split()
@@ -26,7 +30,9 @@ namespace csharpiodemo
             
             while ((data=file.Read(bytes,0,bytes.Length))>0)
             {
+                //newFile.Position = file.Position;
                 bWriter.Write(bytes,0,data);
+                newFile.Flush();
                 //data++;
                 num++;
                 Console.WriteLine(num);
